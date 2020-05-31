1c84f91cc2c15d9d9be48a74233f56b09b7e810c
Radosław Karkut <radoslaw.karkut@getresponse.com>
2020-05-31 09:51:11 +0200

    add Dog class.

---
+++ Dog.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+class Dog
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
